db3fd05810da239f663872e8bbbfc2ece4c4df30
carry0987 <carry0987@users.noreply.github.com>
2020-03-10 21:29:29 +0800

    Update

--- HV/hv_encounter.user.js
+++ HV/hv_encounter.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         ExH EveryDay
+// @name         HV Encounter
 // @author       carry0987
 // @namespace    https://github.com/carry0987
 // @support      https://github.com/carry0987/UserJS/issues
-// @version      1.4.7
-// @description  Get daily bonus reward even in ExHentai & HV
+// @version      1.5.0
+// @description  Auto report when encounter monster
 // @icon         https://carry0987.github.io/favicon.png
 // @match        https://exhentai.org/*
 // @match        https://e-hentai.org/*
@@ -20,6 +20,7 @@ const DAY_MS = 86400 * 1e3;
 const DEBUG = false;
 const six_hr = 2.16e+7;
 const twelve_hr = 4.32e+7;
+var check_encounter = 60000;
 
 class Cookie {
     constructor(cookie = document.cookie) {
@@ -59,7 +60,8 @@ const onload = (resp) => {
         setValue(cookie.id, new Date().toJSON());
     } else if (resp.responseText.match(/Click here to fight in the HentaiVerse/g)) {
         var link = resp.responseText.match(/encounter=(.*?)=/g);
-        setValue('The Encounter', new Date().toJSON());
+        setValue('Final Encounter', new Date().toJSON());
+        countEncounter();
         window.open('https://hentaiverse.org/?s=Battle&ss=ba&'+link[0], '_self');
     }
 }
@@ -68,6 +70,17 @@ const onload = (resp) => {
 function reportInfo(vars, showType = false) {
     if (showType === true) console.log(typeof vars);
     console.log(vars);
+}
+
+//Count total encounter
+function countEncounter() {
+    var get_encounter = getValue('Total Encounter', false);
+    if (get_encounter != null) {
+        setValue('Total Encounter', get_encounter + 1);
+    } else {
+        setValue('Total Encounter', 0);
+    }
+    return get_encounter;
 }
 
 //Get bonus
@@ -111,14 +124,7 @@ function getElem(ele, mode, parent) {
     checkNew();
     function checkNew() {
         setInterval(function() {
-            var dateDiff = Date.now() - lastDate;
-            //reportInfo(six_hr, true);
             getBonus();
-            /*
-            if (dateDiff > DAY_MS || dateDiff === 0) {
-                getBonus();
-            }
-            */
-        }, 5000)
+        }, check_encounter)
     }
 })()
