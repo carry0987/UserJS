d3170d3ad253ceceebb534d380fd5661a2f491a9
carry0987 <carry0987@users.noreply.github.com>
2020-03-11 19:41:43 +0800

    Update hv_encounter.user.js

--- HV/hv_encounter.user.js
+++ HV/hv_encounter.user.js
@@ -3,7 +3,7 @@
 // @author       carry0987
 // @namespace    https://github.com/carry0987
 // @support      https://github.com/carry0987/UserJS/issues
-// @version      1.5.1
+// @version      1.5.2
 // @description  Auto report when encounter monster
 // @icon         https://carry0987.github.io/favicon.png
 // @match        https://exhentai.org/*
@@ -74,8 +74,8 @@ function reportInfo(vars, showType = false) {
 
 //Count total encounter
 function countEncounter() {
-    var get_encounter = getValue('Total Encounter', false);
-    if (get_encounter != null) {
+    var get_encounter = getValue('Total Encounter', true);
+    if (get_encounter != null && (typeof get_encounter) === 'number') {
         setValue('Total Encounter', get_encounter + 1);
     } else {
         setValue('Total Encounter', 0);
