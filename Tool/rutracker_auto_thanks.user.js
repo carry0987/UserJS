8226765b082ff54c44ea212f4413a01870db8072
carry0987 <carry0987@users.noreply.github.com>
2020-03-15 15:20:43 +0800

    Update rutracker_auto_thanks.user.js

--- Tool/rutracker_auto_thanks.user.js
+++ Tool/rutracker_auto_thanks.user.js
@@ -3,7 +3,7 @@
 // @author       carry0987
 // @namespace    https://github.com/carry0987
 // @support      https://github.com/carry0987/UserJS/issues
-// @version      1.1.0
+// @version      1.1.5
 // @description  Auto thanks to the OP when download the torrent
 // @icon         https://carry0987.github.io/favicon.png
 // @match        http*://rutracker.org/forum/viewtopic.php*
@@ -13,23 +13,11 @@
 (function() {
     'use strict';
     //Get value
-    var current_thread = document.querySelector('.attach bordered med');
-    reportInfo(current_thread);
-    var get_price = current_thread.nextSibling.wholeText;
-    get_price = get_price.split(' ')[1].replace(/\,/g, '');
-    get_price = parseInt(get_price);
-    var ask_count = document.getElementById('ask_count');
-    var ask_price = document.getElementById('ask_price');
-
-    let init = function() {
-        ask_count.onchange = function(e) {
-            var get_count = parseInt(ask_count.value);
-            if (get_count >= 1) {
-                ask_price.value = get_price;
-            }
-        }
+    var current_thread = document.querySelector('.dl-stub.dl-link.dl-topic');
+    current_thread.onclick = function() {
+        var thx = getElem('#thx-btn');
+        thx.click();
     }
-    init()
 })();
 
 //Get element
