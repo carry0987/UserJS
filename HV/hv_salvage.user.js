1bea87e74691338d6fc53813e7fbb6ac99fa0492
carry0987 <carry0987@users.noreply.github.com>
2020-03-26 23:46:40 +0800

    Create hv_salvage.user.js

---
+++ HV/hv_salvage.user.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name         HV Salvage
+// @author       carry0987
+// @namespace    https://github.com/carry0987
+// @support      https://github.com/carry0987/UserJS/issues
+// @version      1.0.0
+// @description  Salvage equipment easily
+// @icon         https://carry0987.github.io/favicon.png
+// @match        https://hentaiverse.org/?s=Forge&ss=sa
+// @match        http://alt.hentaiverse.org/?s=Forge&ss=sa
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    //Check if is not in shop
+    if (!getElem('#filterbar')) return;
+    var get_equip = getElem();
+    var materialBox = document.getElementById('repairall');
+    if (materialBox) {
+        const add_box = `
+        <div style="padding: 5px">
+            <span id="auto_buy" style="color: #0382dc; cursor: pointer">Auto Purchase Material And Repair All</span>
+        </div>
+        `;
+        materialBox.parentElement.insertAdjacentHTML('afterbegin', add_box);
+    }
+    var getBox = document.getElementById('auto_buy');
+    if (getBox) {
+        getBox.onclick = function() {
+            var confirm_purchase = confirm('Total Cost: '+getTotalCost(material));
+            if (confirm_purchase == true) {
+                repairEqupiment(material);
+            } else {
+                return;
+            }
+        }
+    }
+})()
+
+//Get material name
+function materialsName2Code(name) {
+    switch (name) {
+        case 'Scrap Cloth':
+            return '0';
+        case 'Scrap Leather':
+            return '1';
+        case 'Scrap Metal':
+            return '2';
+        case 'Scrap Wood':
+            return '3';
+        case 'Energy Cell':
+            return '4';
+    }
+}
+
+//Purchase materials
+function buyMaterial(code, amount, cost, token) {
+    var xhr = 'xhr_Buy' + Math.random().toString();
+    xhr = new window.XMLHttpRequest();
+    xhr.open('POST', window.location.origin + '/?s=Bazaar&ss=is&filter=ma');
+    //shop_pane: Buy, item_pane: Sell
+    var parm = 'storetoken=' + token + '&select_mode=shop_pane&select_item=' + code + '&select_count=' + amount;
+    xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(parm);
+    xhr.onload = function() {}
+}
+
+//Repair all equipment
+function repairEqupiment(material) {
+    var materialsList = document.querySelectorAll('#repairall+div span');
+    var xhr = new window.XMLHttpRequest();
+    xhr.open('GET', window.location.origin + '/?s=Bazaar&ss=is&filter=ma');
+    xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.responseType = 'document';
+    xhr.onload = function() {
+        var token = xhr.response.querySelector('input[name="storetoken"]').value;
+        if (materialsList.length > 0) {
+            for (var i = 0; i < materialsList.length; i++) {
+                var amount = materialsList[i].innerHTML.match(/\d+/)[0];
+                var code = material[materialsName2Code(materialsList[i].innerHTML.match(/\d+x (.*)/)[1])].code;
+                buyMaterial(code, amount, material[i].cost, token);
+            }
+            setTimeout(function() {
+                document.querySelector('#repairall div').click();
+            }, 1000)
+        }
+    }
+    xhr.send(null)
+}
+
+//Get total cost of material
+function getTotalCost(material) {
+    var materialsList = document.querySelectorAll('#repairall+div span');
+    var total = 0;
+    if (materialsList.length > 0) {
+        for (var i = 0; i < materialsList.length; i++) {
+            var amount = materialsList[i].innerHTML.match(/\d+/)[0];
+            var code = material[materialsName2Code(materialsList[i].innerHTML.match(/\d+x (.*)/)[1])].code;
+            total += amount*material[i].cost;
+        }
+    }
+    return total;
+}
+
+//Get element
+function getElem(ele, mode, parent) {
+    if (typeof ele === 'object') {
+        return ele;
+    } else if (mode === undefined && parent === undefined) {
+        return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
+    } else if (mode === 'all') {
+        return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
+    } else if (typeof mode === 'object' && parent === undefined) {
+        return mode.querySelector(ele);
+    }
+}
