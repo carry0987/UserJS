e30330d83f7a5da5313d12a752fc50c770e70d83
carry0987 <carry0987@users.noreply.github.com>
2020-04-08 22:25:24 +0800

    Update

--- HV/hv_chinese_translation.user.js
+++ HV/hv_chinese_translation.user.js
@@ -3,7 +3,7 @@
 // @author       ggxxsol(ggxxhy); mbbdzz; hc br; carry0987
 // @namespace    https://github.com/carry0987
 // @support      https://github.com/carry0987/UserJS/issues
-// @version      1.1.1
+// @version      1.1.2
 // @description  Translate HentaiVerse items into traditional chinese
 // @icon         https://e-hentai.org/favicon.ico
 // @include      https://hentaiverse.org/*
